c69ba37f8a3e9af032b8362df539e624ee0ebc36
Suhag Al Amin <developersuhag@gmail.com>
2021-10-23 18:57:52 +0600

    delete

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -8,14 +8,37 @@ const Products = () => {
       .then((data) => setProdcuts(data));
   }, []);
 
+  const handleDelete = (id) => {
+    const proceed = window.confirm(
+      "Are you sure? If you delete you can not revert it!"
+    );
+
+    if (proceed) {
+      const url = `http://localhost:5000/products/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("delete success");
+            const remaining = products.filter((p) => p._id !== id);
+            setProdcuts(remaining);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h3>products found: {products.length}</h3>
       <ul>
         {products.map((p) => (
-          <li>
+          <li key={p._id}>
             <strong>{p.productName}</strong>, Price:$ {p.productPrice}::
             Quantity: {p.productQuantity}
+            <button>Update</button>
+            <button onClick={() => handleDelete(p._id)}>X</button>
           </li>
         ))}
       </ul>
